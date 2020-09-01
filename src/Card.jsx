4c6a3bf17b699c41d031314821480d3d50cf497d
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-09-01 21:43:02 +0530

    add desing

--- src/Card.jsx
+++ src/Card.jsx
@@ -4,13 +4,13 @@ function Card(prop)
 {
     return(
         <>
-        <div className="cards">
+        <div className={prop.class}>
           <div className="card">
               <img src={prop.imagesrc} alt="my image" className="card_img"></img>
               <div className="card_info">
                 <span className="card_cat">{prop.title}</span>
                 <h3 className="card_details">{prop.sname}</h3>
-                <a href={prop.link} target="_blank">
+                <a href={prop} target="_blank">
                   <button>Watch Now</button>
                 </a>  
               </div>
--- src/index.js
+++ src/index.js
@@ -5,24 +5,29 @@ import {Card} from './Card';
 
 ReactDOM.render(
   <>
-    <Card
-      imagesrc="https://picsum.photos/200/300" 
-      title="An Amazon Web Series" 
-      sname="Family Man"
-      link="https://www.amazon.com/gp/video/detail/B08BYZMXSP/ref=atv_hm_hom_3_c_0z7Q22_brws_2_1"
-    />
-    <Card
-      imagesrc="https://picsum.photos/id/237/200/300" 
-      title="An Amazon Web Series" 
-      sname="Hidden Secret"
-      link="https://www.amazon.com/gp/video/detail/B08DK1G84T/ref=atv_hm_hom_3_c_0z7Q22_brws_2_3"
-    />
-    <Card
-      imagesrc="https://picsum.photos/seed/picsum/200/300" 
-      title="An Amazon Web Series" 
-      sname="Patal Lok"
-      link="https://www.amazon.com/Badlands/dp/B08BYYXFJL/ref=sr_1_1?dchild=1&keywords=Patal+lok&qid=1598974679&s=instant-video&sr=1-1"
-    />
+  <div className="grid">
+      <Card
+        class="card1"
+        imagesrc="https://picsum.photos/200/300" 
+        title="An Amazon Web Series" 
+        sname="Family Man"
+        link="https://www.amazon.com/gp/video/detail/B08BYZMXSP/ref=atv_hm_hom_3_c_0z7Q22_brws_2_1"
+      />
+      <Card
+        class="card2"
+        imagesrc="https://picsum.photos/id/237/200/300" 
+        title="An Amazon Web Series" 
+        sname="Hidden Secret"
+        link="https://www.amazon.com/gp/video/detail/B08DK1G84T/ref=atv_hm_hom_3_c_0z7Q22_brws_2_3"
+      />
+      <Card
+        class="card3"
+        imagesrc="https://picsum.photos/seed/picsum/200/300" 
+        title="An Amazon Web Series" 
+        sname="Patal Lok"
+        link="https://www.amazon.com/Badlands/dp/B08BYYXFJL/ref=sr_1_1?dchild=1&keywords=Patal+lok&qid=1598974679&s=instant-video&sr=1-1"
+      />
+  </div>
   </>,
   document.getElementById('root')
 );
