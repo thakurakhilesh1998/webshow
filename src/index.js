08902e19fbb888e283c6983b7dd2d266fbba4442
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-09-02 21:14:31 +0530

    final

--- src/index.js
+++ src/index.js
@@ -4,49 +4,26 @@ import './index.css';
 import {Card} from './Card';
 import {webData} from './webdata';
 
-console.log(webData[0]);
-
 ReactDOM.render(
   <>
   <h1 className="headings">
     Netflix Show
   </h1>
   <div className="grid">
-      <Card
-        class={webData[0].class}
-        imagesrc={webData[0].imagesrc}
-        title={webData[0].title} 
-        sname={webData[0].sname}
-        link={webData[0].link}
-      />
-       <Card
-        class={webData[1].class}
-        imagesrc={webData[1].imagesrc}
-        title={webData[1].title} 
-        sname={webData[1].sname}
-        link={webData[0].link}
-      />
-       <Card
-        class={webData[2].class}
-        imagesrc={webData[2].imagesrc}
-        title={webData[2].title} 
-        sname={webData[2].sname}
-        link={webData[2].link}
-      />
-       <Card
-        class={webData[3].class}
-        imagesrc={webData[3].imagesrc}
-        title={webData[3].title} 
-        sname={webData[3].sname}
-        link={webData[3].link}
-      />
-       <Card
-        class={webData[4].class}
-        imagesrc={webData[4].imagesrc}
-        title={webData[4].title} 
-        sname={webData[4].sname}
-        link={webData[4].link}
-      />
+  
+  { 
+    webData.map((data,index)=>{
+      return (
+        <Card
+    class={data.class}
+    imagesrc={data.imagesrc}
+    title={data.title} 
+    sname={data.sname}
+    link={data.link}
+  />
+)
+  }) }
+
   </div>
   </>,
   document.getElementById('root')
